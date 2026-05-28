5902deed865238a5992cd653455fdd535a612853
victor <vlopez@esliceu.net>
2026-05-28 13:26:40 +0200

    Crear scripts

---
+++ script2.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+variables són "paraules" que contenen un valor
+NOM="Miquel"
+read serveix per llegir informació del teclat
+read -p "introdueix un nom: " NOM
+echo "El teu nom és ${NOM}"
